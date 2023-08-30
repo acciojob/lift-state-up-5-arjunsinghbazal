321b4be71b6a4de6bba2af93f80dd561ff39d6d5
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-08-30 12:10:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 
-import React from "react";
+
 import './../styles/App.css';
+import React, {useState} from "react";
+
+
+const Parent = ()=>{
+    let [isLoggedIn, setisLoggedIn] = useState(false);
+
+    function changeStatus(){
+        setisLoggedIn(true);
+    }
+
+    return(
+        <div>
+            <h1>Parent Component</h1>
+            {
+                isLoggedIn?(<p>You are logged in!</p>):(<Child setStatus={changeStatus}></Child>)
+            }
+        </div>
+    )
+}
+const Child = ({setStatus})=>{
+    
+    return(
+        <form>
+            <label>Username:</label>
+            <input type="text" ></input>
+            <br></br>
+            <br></br>
+            <label>Password:</label>
+            <input type="password" ></input>
+            <br></br>
+            <br></br>
+            <button onClick={()=>{setStatus()}}>Login</button>
+        </form>
+    )
+}
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Parent/>
     </div>
   )
 }
